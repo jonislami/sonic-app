c65cbe550ee75ddd6b412527e62f926e9f675252
jonislami <102537752+jonislami@users.noreply.github.com>
2026-01-19 18:07:40 +0100

    Update route.ts

--- src/app/api/servise/route.ts
+++ src/app/api/servise/route.ts
@@ -1,6 +1,9 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const q = (url.searchParams.get("q") ?? "").trim();
